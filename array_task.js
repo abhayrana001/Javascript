6f8559868cc417ae5a98bc861b0ada8b507431de
abhayrana001 <abhayrana0290@gmail.com>
2025-01-09 22:55:14 +0530

    array task 1

---
+++ array_task.js
@@ -0,0 +1,16 @@
+// task to print the following using 2 arrays
+// arr1 =[11,22,33,44,55]
+// arr2 =[66,77,88,99,100]
+
+// 11 66
+// 22 77
+// 33 88
+// 44 99
+// 55 100
+
+let arr1 = [11,22,33,44,55]
+let arr2 = [66,77,88,99,100]
+
+for(let i=0;i<arr1.length;i++){
+    console.log(arr1[i],arr2[i]);
+}
