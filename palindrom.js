bcc0a26b4a1f7c280849575feac33b057e899e9c
abhayrana001 <abhayrana0290@gmail.com>
2025-01-10 22:21:27 +0530

    palindrom file added

---
+++ palindrom.js
@@ -0,0 +1,14 @@
+let s = "abcdcba";
+let temp;
+for(let i=0; i<s.length/2; i++){
+    if(s[i]!=s[s.length-i-1]){
+        temp = 1;
+        console.log("not palindrom")
+        break;
+    }
+    else
+    temp = 0;
+}
+if(temp == 0){
+    console.log("palindrom");
+}
