d2d5d0cb57ae17e020f09ee6b082f161bae69540
abhayrana001 <abhayrana0290@gmail.com>
2025-01-14 23:06:23 +0530

    use inbuild methods

--- reversing_string.js
+++ reversing_string.js
@@ -1,7 +1,12 @@
 // reversing a string using for loop
-let str = "abhay";
-let new_str = " ";
+let str = "react";
+let new_str = "";
 for(let i=str.length-1; i>=0; i--){
     new_str = new_str + str[i];
 }
 console.log(new_str);
+
+// reversing a string using inbuilt methods
+let n = "javascript";
+let result = n.split("").reverse().join("");
+console.log(result);
