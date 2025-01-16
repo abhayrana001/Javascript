17622e1eafbd65e3cb467d0720169f27ee94ffb9
abhayrana001 <abhayrana0290@gmail.com>
2025-01-16 23:41:47 +0530

    pattern file added

---
+++ interview_pattern.js
@@ -0,0 +1,19 @@
+let n = 3;
+// for number of rows
+for (let i = 1; i <= n; i++) {
+    let row = " ";
+    // for space 
+    for (let j = 1; j <= n - i; j++) {
+        row = row + " ";
+    }
+    // for printing *
+    for (let k = 1; k <= (2 * i - 1); k++) {
+        if (i == n || k == 1 || k == (2 * i - 1)) {
+            row = row + "*";
+        }
+        else {
+            row += " ";
+        }
+    }
+    console.log(row);
+}
