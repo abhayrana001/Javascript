82f70199eafe6ac278a359ac3b8f3b30d1db38e6
abhayrana001 <abhayrana0290@gmail.com>
2025-01-20 16:52:01 +0530

    slice method added

---
+++ Array_slice.js
@@ -0,0 +1,8 @@
+// Beginner Task:
+// Task:
+// Given an array numbers = [10, 20, 30, 40, 50, 60], use the slice() method to create a new array that contains the first 3 elements.
+// Expected Output: [10, 20, 30]
+
+const numbers = [10, 20, 30, 40, 50, 60];
+const firstThree = numbers.slice(0, 3);
+console.log(firstThree);  // Output: [10, 20, 30]
