c07abdd8e1f06a720b3953d605cfd5042938b37b
abhayrana001 <abhayrana0290@gmail.com>
2025-01-29 16:27:25 +0530

    string task added

--- String_Slice.js
+++ String_Slice.js
@@ -2,3 +2,9 @@
 const str = "Hello, World!";
 const slicedStr = str.slice(0, 5);
 console.log(slicedStr); // Output: "Hello"
+
+// Task: Given the string "JavaScript is awesome!", extract only the word "awesome" using slice().
+const text = "JavaScript is awesome!";
+const slicedText = text.slice(14, 21);  // "awesome" starts at index 15 and ends at 21 (22 is excluded)
+console.log(slicedText); // Output: "awesome"
+
