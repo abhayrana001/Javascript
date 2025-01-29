af3ce25740080d57863c0818f0666b84cea24032
abhayrana001 <abhayrana0290@gmail.com>
2025-01-29 16:24:28 +0530

    string slice added

---
+++ String_Slice.js
@@ -0,0 +1,4 @@
+// Task: Extract the first 5 characters from the string "Hello, World!".
+const str = "Hello, World!";
+const slicedStr = str.slice(0, 5);
+console.log(slicedStr); // Output: "Hello"
