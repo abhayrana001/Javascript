6255907711ed6744df624dc154b5642f7e496d18
abhayrana001 <abhayrana0290@gmail.com>
2025-02-08 18:28:36 +0530

    second task added

--- String_Split.js
+++ String_Split.js
@@ -5,3 +5,10 @@
 const fruits = "apple,banana,grape,orange";
 const fruitArray = fruits.split(",");
 console.log(fruitArray);
+
+// Task: Given the string "Hello World! JavaScript is fun.", split it into an array of words using space as the delimiter.
+
+// Expected Output: ['Hello', 'World!', 'JavaScript', 'is', 'fun.']
+const sentence = "Hello World! JavaScript is fun.";
+const wordsArray = sentence.split(" ");
+console.log(wordsArray);
