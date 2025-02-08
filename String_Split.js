4d7caa285ee1da98561ed39e3dedef1d4964588e
abhayrana001 <abhayrana0290@gmail.com>
2025-02-08 18:27:11 +0530

    string split task added

---
+++ String_Split.js
@@ -0,0 +1,7 @@
+// Task: Given the string "apple,banana,grape,orange", split it into an array of fruits using split().
+
+// Expected Output: ['apple', 'banana', 'grape', 'orange']
+
+const fruits = "apple,banana,grape,orange";
+const fruitArray = fruits.split(",");
+console.log(fruitArray);
