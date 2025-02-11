e05b5263e605c63bf240b757a130e38e681fe218
abhayrana001 <abhayrana0290@gmail.com>
2025-02-11 18:04:58 +0530

    task added

--- Set.js
+++ Set.js
@@ -4,3 +4,13 @@
 const numbers = [1, 2, 2, 3, 4, 4, 5];
 const uniqueNumbers = new Set(numbers);
 console.log(uniqueNumbers);
+
+// Task: Create a function removeDuplicates(arr) that removes duplicates from an array using a Set.
+
+// Expected Output: [1, 2, 3, 4, 5].
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+  }
+  
+  console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+  
