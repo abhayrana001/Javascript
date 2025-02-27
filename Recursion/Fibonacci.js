0f9928fda4316ad2fd05e38336b0b02d628d140a
abhayrana001 <abhayrana0290@gmail.com>
2025-02-27 18:16:34 +0530

    task correction

--- Recursion/Fibonacci.js
+++ Recursion/Fibonacci.js
@@ -2,5 +2,5 @@ function fibonacci(n) {
     return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
   }
   
-  console.log(fibonacci(6)); // 8
+  console.log(`6th number of series is`,fibonacci(6)); // 8
   
